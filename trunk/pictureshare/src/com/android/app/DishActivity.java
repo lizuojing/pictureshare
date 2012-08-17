5c9aa6943a9217ee198c8fe4da71f0ba507d81da
lizuojing42@gmail.com <lizuojing42@gmail.com@d6340a6c-02dd-5795-9046-c8390326212e>
2012-08-17 02:26:47 +0000

    add dish

---
+++ trunk/pictureshare/src/com/android/app/DishActivity.java
@@ -0,0 +1,93 @@
+package com.android.app;
+
+import android.app.ActivityGroup;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.android.app.view.FanShapedView;
+
+/**
+ * 拍照页面
+ * 
+ * @author Administrator
+ * 
+ */
+public class DishActivity extends ActivityGroup {
+	private static final String TAG = "PicTakeActivity";
+	private ImageView imageView;
+	private String filePath;
+	private LinearLayout eachLayout;
+	private FanShapedView fanShapedView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		filePath = getIntent().getStringExtra("mCurrentFile");
+		Log.i(TAG, "filePath is " + filePath);
+
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.dish);
+
+		eachLayout = (LinearLayout) findViewById(R.id.each_layout);
+		fanShapedView = (FanShapedView) findViewById(R.id.fanshaped);
+
+		eachLayout.addView(getLocalActivityManager().startActivity(
+				"contact",
+				new Intent(this, PicTakeActivity.class)
+						.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+				.getDecorView());
+
+		setListener();
+
+		// initComponents();
+		// updateUI();
+
+	}
+
+	private void setListener() {
+		fanShapedView.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (fanShapedView.isStartActivity()) {
+					String module = fanShapedView.getFirstItem();
+					Intent intent = new Intent();
+					intent.setAction(PicTakeActivity.ACTION_3D);
+					intent.putExtra("orientation", FanShapedView.orient);
+					intent.putExtra("module", module);
+					sendBroadcast(intent);
+				}
+			}
+		});
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		fanShapedView.recycle();
+		System.exit(0);
+	}
+
+	private void updateUI() {
+		Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+		imageView.setImageBitmap(bitmap);
+	}
+
+	private void initComponents() {
+		// imageView = (ImageView)findViewById(R.id.media_image);
+
+	}
+}
