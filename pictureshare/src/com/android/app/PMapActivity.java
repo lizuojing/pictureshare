97a964686a9b0201f807fbc2de9b1dc60b94a6e7
lizuojing42@gmail.com <lizuojing42@gmail.com@d6340a6c-02dd-5795-9046-c8390326212e>
2012-09-22 15:43:09 +0000

    modify PMapActivity.java

--- pictureshare/src/com/android/app/PMapActivity.java
+++ pictureshare/src/com/android/app/PMapActivity.java
@@ -33,9 +33,12 @@ import com.baidu.mapapi.MyLocationOverlay;
 import com.baidu.mapapi.Overlay;
 import com.baidu.mapapi.Projection;
 
-public class PMapActivity extends MapActivity implements View.OnClickListener{
+public class PMapActivity extends MapActivity implements View.OnClickListener {
 	protected static final String TAG = "PMapActivity";
 	protected static final int ID_DELETE_OVERLAY = 2000;
+	public static final int STATE_CREATE = 0;
+	public static final int STATE_DEL = 1;
+	public static final int STATE_POINT = 2;
 	private BMapManager mBMapMan;
 	private MKLocationManager mLocationManager;
 	public static MapView mMapView;
@@ -45,127 +48,111 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 	private Button detalButton;
 	private Button backToOneButton;
 	private Button backButton;
-	
+
 	private boolean isDelete = false;
 	private SharePopupWindow sharePopup;
 	private OverItemT overitem;
+
+	public static int currentStatus = 0;// 0代表新建 1 代表删除
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.map);
 		PicService.allActivity.add(this);
-		
 
 		mBMapMan = new BMapManager(getApplication());
-		mBMapMan.init("1F572AAE2DC844C03D5AF0C9A001026E03BD1618", new MKGeneralListener() {
-		    @Override
-		    public void onGetPermissionState(int iError) {
-		        //  返回授权验证错误，通过错误代码判断原因，MKEvent中常量值。
-		    	Log.i(TAG, "permission error is " + iError);
-		    }
-		    @Override
-		    public void onGetNetworkState(int iError) {
-		        // 返回网络错误，通过错误代码判断原因，MKEvent中常量值。
-		    	Log.i(TAG, "Network error is " + iError);
-		    }
-		});
+		mBMapMan.init("1F572AAE2DC844C03D5AF0C9A001026E03BD1618",
+				new MKGeneralListener() {
+					@Override
+					public void onGetPermissionState(int iError) {
+						// 返回授权验证错误，通过错误代码判断原因，MKEvent中常量值。
+						Log.i(TAG, "permission error is " + iError);
+					}
+
+					@Override
+					public void onGetNetworkState(int iError) {
+						// 返回网络错误，通过错误代码判断原因，MKEvent中常量值。
+						Log.i(TAG, "Network error is " + iError);
+					}
+				});
 		super.initMapActivity(mBMapMan);
-		
+
 		mMapView = (MapView) findViewById(R.id.bmapsView);
 		mMapView.setBuiltInZoomControls(true); // 设置启用内置的缩放控件
 		mMapView.setDoubleClickZooming(false);
-		
-/*		mMapView.setOnTouchListener(new OnTouchListener() {
-			
-			private float startX;
-			private float startY;
-			private float endX;
-			private float endY;
 
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				switch (event.getAction()) {
-				case MotionEvent.ACTION_DOWN:
-					Log.i(TAG, "start x is " + event.getX() + " start y is " + event.getY());
-					startX = event.getX();
-					startY = event.getY();
-					break;
-				case MotionEvent.ACTION_MOVE:
-					
-					break;
-				case MotionEvent.ACTION_UP:
-					Log.i(TAG, "end x is " + event.getX() + " end y is " + event.getY());
-					endX = event.getX();
-					endY = event.getY();
-
-					break;
-
-				default:
-					break;
-				}
-				
-				if(!isDelete) {//新建
-					if(MotionEvent.ACTION_UP==event.getAction()) {
-						if((endX-startX)==0&&(endY-startY)==0) {
-							MapView map = (MapView)v;
-							Projection projection = map.getProjection();
-							GeoPoint fromPixels = projection.fromPixels((int)endX, (int)endY);
-							map.getOverlays().add(new MyOverlay(fromPixels));
-						}
-					}
-				}else {
-					//删除覆盖物
-					showDialog(ID_DELETE_OVERLAY);
-				}
-				return false;
-			}
-		});*/
-		
+		/*
+		 * mMapView.setOnTouchListener(new OnTouchListener() {
+		 * 
+		 * private float startX; private float startY; private float endX;
+		 * private float endY;
+		 * 
+		 * @Override public boolean onTouch(View v, MotionEvent event) { switch
+		 * (event.getAction()) { case MotionEvent.ACTION_DOWN: Log.i(TAG,
+		 * "start x is " + event.getX() + " start y is " + event.getY()); startX
+		 * = event.getX(); startY = event.getY(); break; case
+		 * MotionEvent.ACTION_MOVE:
+		 * 
+		 * break; case MotionEvent.ACTION_UP: Log.i(TAG, "end x is " +
+		 * event.getX() + " end y is " + event.getY()); endX = event.getX();
+		 * endY = event.getY();
+		 * 
+		 * break;
+		 * 
+		 * default: break; }
+		 * 
+		 * if(!isDelete) {//新建 if(MotionEvent.ACTION_UP==event.getAction()) {
+		 * if((endX-startX)==0&&(endY-startY)==0) { MapView map = (MapView)v;
+		 * Projection projection = map.getProjection(); GeoPoint fromPixels =
+		 * projection.fromPixels((int)endX, (int)endY);
+		 * map.getOverlays().add(new MyOverlay(fromPixels)); } } }else { //删除覆盖物
+		 * showDialog(ID_DELETE_OVERLAY); } return false; } });
+		 */
 
 		MapController mMapController = mMapView.getController(); // 得到mMapView的控制权,可以用它控制和驱动平移和缩放
 		GeoPoint point = new GeoPoint((int) (39.915 * 1E6),
 				(int) (116.404 * 1E6)); // 用给定的经纬度构造一个GeoPoint，单位是微度 (度 * 1E6)
 		mMapController.setCenter(point); // 设置地图中心点
 		mMapController.setZoom(12); // 设置地图zoom级别
-//		mMapView.setSatellite(true);
-		
+		// mMapView.setSatellite(true);
+
 		// 初始化Location模块
 		mLocationManager = mBMapMan.getLocationManager();
 		// 通过enableProvider和disableProvider方法，选择定位的Provider
 		// mLocationManager.enableProvider(MKLocationManager.MK_NETWORK_PROVIDER);
 		// mLocationManager.disableProvider(MKLocationManager.MK_GPS_PROVIDER);
-		
+
 		// 添加定位图层
 		MyLocationOverlay mylocTest = new MyLocationOverlay(this, mMapView);
 		mylocTest.enableMyLocation(); // 启用定位
-		mylocTest.enableCompass();    // 启用指南针
+		mylocTest.enableCompass(); // 启用指南针
 		mMapView.getOverlays().add(mylocTest);
-		
+
 		createPopView();
-		
+
 		initComponents();
 		updateUI();
 	}
 
 	private void createPopView() {
-		if(PicApp.list!=null&&PicApp.list.size()<=0) {
+		if (PicApp.list != null && PicApp.list.size() <= 0) {
 			return;
 		}
-		
+
 		// 添加ItemizedOverlay
-		Drawable marker = getResources().getDrawable(R.drawable.iconmarka);  //得到需要标在地图上的资源
+		Drawable marker = getResources().getDrawable(R.drawable.iconmarka); // 得到需要标在地图上的资源
 		marker.setBounds(0, 0, marker.getIntrinsicWidth(), marker
-				.getIntrinsicHeight());   //为maker定义位置和边界
-		
+				.getIntrinsicHeight()); // 为maker定义位置和边界
+
 		overitem = new OverItemT(marker, this, PicApp.list);
-		mMapView.getOverlays().add(overitem); //添加ItemizedOverlay实例到mMapView
-		
+		mMapView.getOverlays().add(overitem); // 添加ItemizedOverlay实例到mMapView
+
 		// 创建点击mark时的弹出泡泡
-		mPopView=super.getLayoutInflater().inflate(R.layout.popview, null);
-		mMapView.addView( mPopView,
-                new MapView.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT,
-                		null, MapView.LayoutParams.TOP_LEFT));
+		mPopView = super.getLayoutInflater().inflate(R.layout.popview, null);
+		mMapView.addView(mPopView, new MapView.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, null,
+				MapView.LayoutParams.TOP_LEFT));
 		mPopView.setVisibility(View.GONE);
 	}
 
@@ -176,9 +163,14 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 		}
 		super.onStart();
 	}
-	
+
 	private void updateUI() {
-		if(!isDelete) {
+//		if (!isDelete) {
+//			deleteButton.setText(getResources().getString(R.string.create));
+//		} else {
+//			deleteButton.setText(getResources().getString(R.string.delete));
+//		}
+		if(currentStatus==STATE_CREATE) {
 			deleteButton.setText(getResources().getString(R.string.create));
 		}else {
 			deleteButton.setText(getResources().getString(R.string.delete));
@@ -186,12 +178,12 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 	}
 
 	private void initComponents() {
-		backButton = (Button)findViewById(R.id.btn_back);
-		deleteButton = (Button)findViewById(R.id.btn_create_or_delete);
-		shareButton = (Button)findViewById(R.id.btn_share);
-		detalButton = (Button)findViewById(R.id.btn_detail);
-		backToOneButton = (Button)findViewById(R.id.btn_goback);
-		
+		backButton = (Button) findViewById(R.id.btn_back);
+		deleteButton = (Button) findViewById(R.id.btn_create_or_delete);
+		shareButton = (Button) findViewById(R.id.btn_share);
+		detalButton = (Button) findViewById(R.id.btn_detail);
+		backToOneButton = (Button) findViewById(R.id.btn_goback);
+
 		backButton.setOnClickListener(this);
 		deleteButton.setOnClickListener(this);
 		shareButton.setOnClickListener(this);
@@ -203,7 +195,7 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 	protected boolean isRouteDisplayed() {
 		return false;
 	}
-	
+
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		PicDialog dialog = null;
@@ -231,7 +223,6 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 		}
 		return super.onCreateDialog(id);
 	}
-	
 
 	@Override
 	protected void onDestroy() {
@@ -257,20 +248,26 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 		}
 		super.onResume();
 	}
-	
+
 	private void showSharePopup() {
-		String picUrl = null;//TODO 需要获取要分享的url
+		String picUrl = null;// TODO 需要获取要分享的url
 		if (sharePopup == null) {
-			sharePopup = new SharePopupWindow(this,picUrl, R.drawable.bg_share_popup_1);
+			sharePopup = new SharePopupWindow(this, picUrl,
+					R.drawable.bg_share_popup_1);
 		}
 		int[] btnShareLoc = new int[2];
 		deleteButton.getLocationInWindow(btnShareLoc);
-//		int offX = btnShareLoc[0] - Utils.dipToPixels(this, 6);
-		int offY = Utils.getScreenHeight(this) - btnShareLoc[1] + Utils.dipToPixels(this, 5);
+		// int offX = btnShareLoc[0] - Utils.dipToPixels(this, 6);
+		int offY = Utils.getScreenHeight(this) - btnShareLoc[1]
+				+ Utils.dipToPixels(this, 5);
 		int orientation = getResources().getConfiguration().orientation;
 		sharePopup.show(deleteButton, orientation, 0, offY);
 	}
-	
+
+	public static int getCurrentStatus() {
+		return currentStatus;
+	}
+
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -279,16 +276,26 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 			break;
 		case R.id.btn_create_or_delete:
 			isDelete = !isDelete;
-			if(!isDelete) {
+			if (!isDelete) {
+				currentStatus = STATE_CREATE;
+//				deleteButton.setText(getResources().getString(R.string.create));
+			} else {
+				currentStatus = STATE_DEL;
+//				deleteButton.setText(getResources().getString(R.string.delete));
+			}
+			
+			
+			
+			if(currentStatus==STATE_CREATE) {
 				deleteButton.setText(getResources().getString(R.string.create));
 			}else {
 				deleteButton.setText(getResources().getString(R.string.delete));
 			}
-//			Toast.makeText(this, "删除", Toast.LENGTH_SHORT).show();
+			// Toast.makeText(this, "删除", Toast.LENGTH_SHORT).show();
 			break;
 		case R.id.btn_goback:
-//			Toast.makeText(this, "回到一级", Toast.LENGTH_SHORT).show();
-			Intent intent = new Intent(this,MainActivity.class);
+			// Toast.makeText(this, "回到一级", Toast.LENGTH_SHORT).show();
+			Intent intent = new Intent(this, MainActivity.class);
 			startActivity(intent);
 			break;
 		case R.id.btn_share:
@@ -296,13 +303,12 @@ public class PMapActivity extends MapActivity implements View.OnClickListener{
 			break;
 		case R.id.btn_detail:
 			Toast.makeText(this, "detal", Toast.LENGTH_SHORT).show();
-			//发送左上右下坐标
+			// 发送左上右下坐标
 			break;
 
-			
 		default:
 			break;
 		}
-		
+
 	}
 }
