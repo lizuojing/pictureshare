cab363f057f0ae0600813409d8b5835c8b4294bf
lizuojing42@gmail.com <lizuojing42@gmail.com@d6340a6c-02dd-5795-9046-c8390326212e>
2012-09-28 14:07:46 +0000

    修改设置页面

--- pictureshare/src/com/android/app/SettingActivity.java
+++ pictureshare/src/com/android/app/SettingActivity.java
@@ -12,11 +12,8 @@ import android.widget.Toast;
  *
  */
 public class SettingActivity extends BaseActivity implements OnClickListener{
-	private RelativeLayout svrSetting;
 	private RelativeLayout personalSetting;
-	private RelativeLayout picSetting;
 	private RelativeLayout valuationSetting;
-	private RelativeLayout commonSetting;
 	private Button backButton;
 
 	@Override
@@ -31,18 +28,12 @@ public class SettingActivity extends BaseActivity implements OnClickListener{
 	private void initComponents() {
 		
 		backButton = (Button)findViewById(R.id.button1);
-		svrSetting = (RelativeLayout)findViewById(R.id.svr_setting);
 		personalSetting = (RelativeLayout)findViewById(R.id.personal_setting);
-		picSetting = (RelativeLayout)findViewById(R.id.pic_setting);
 		valuationSetting = (RelativeLayout)findViewById(R.id.valuation_setting);
-		commonSetting = (RelativeLayout)findViewById(R.id.common_setting);
 		
 		backButton.setOnClickListener(this);
-		svrSetting.setOnClickListener(this);
 		personalSetting.setOnClickListener(this);
-		picSetting.setOnClickListener(this);
 		valuationSetting.setOnClickListener(this);
-		commonSetting.setOnClickListener(this);
 	}
 
 	@Override
@@ -51,20 +42,11 @@ public class SettingActivity extends BaseActivity implements OnClickListener{
 		case R.id.button1:
 			super.onBackPressed();
 			break;
-		case R.id.svr_setting:
-			Toast.makeText(this, "服务器设置", Toast.LENGTH_SHORT).show();
-			break;
 		case R.id.personal_setting:
 			Toast.makeText(this, "个人中心", Toast.LENGTH_SHORT).show();
 			break;
-		case R.id.pic_setting:
-			Toast.makeText(this, "图片设置", Toast.LENGTH_SHORT).show();
-			break;
 		case R.id.valuation_setting:
 			Toast.makeText(this, "评价", Toast.LENGTH_SHORT).show();
-			break;
-		case R.id.common_setting:
-			Toast.makeText(this, "通用", Toast.LENGTH_SHORT).show();
 			break;
 
 		default:
