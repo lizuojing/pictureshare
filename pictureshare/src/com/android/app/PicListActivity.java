ca4b7f4dbb12ddd65be1755f77a4cc4ef5da426c
lizuojing42@gmail.com <lizuojing42@gmail.com@d6340a6c-02dd-5795-9046-c8390326212e>
2012-11-10 12:32:56 +0000

    修改二级列表页面

--- pictureshare/src/com/android/app/PicListActivity.java
+++ pictureshare/src/com/android/app/PicListActivity.java
@@ -2,10 +2,12 @@ package com.android.app;
 
 import java.util.ArrayList;
 
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -14,22 +16,33 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
+import com.android.app.api.ApiResult;
+import com.android.app.api.ApiReturnResultListener;
+import com.android.app.api.AvatarApi;
+import com.android.app.api.AvatarRequestParam;
+import com.android.app.data.SettingLoader;
 import com.android.app.entity.Avatar;
+import com.android.app.entity.Location;
 import com.android.app.image.ImageLoaderManager;
 import com.android.app.utils.Utils;
 import com.android.app.view.CellItem;
 
 public class PicListActivity extends BaseActivity implements View.OnClickListener{
 	
+	protected static final String TAG = "PicListActivity";
 	private ListView listView;
 	private CheckBox editButton;
 	private ArrayList<Avatar> list;
 	private Button backButton;
-	private DataLoaderTask loadDataTask;
 	private ProgressBar mProgresssBar;
+	private ArrayList<String> pointlist;
+	private String picTitle;
+	private ListAdapter adapter;
+	private TextView tiptext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,42 +51,133 @@ public class PicListActivity extends BaseActivity implements View.OnClickListene
 		setContentView(R.layout.piclist);
 		initComponents();
 		
-		if(loadDataTask!=null) {
-			loadDataTask.cancel(true);
-			loadDataTask = null;
-		}
-		loadDataTask = new DataLoaderTask();
+		picTitle = getIntent().getStringExtra("picTitle");
+		pointlist = getIntent().getStringArrayListExtra("list");
+		uploadPhotoInfo();
 	}
 	
-	final class DataLoaderTask extends AsyncTask<Void, Integer, Long> {
-
-		@Override
-		protected Long doInBackground(Void... params) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		protected void onPostExecute(Long result) {
-			mProgresssBar.setVisibility(View.GONE);
-			listView.setVisibility(View.VISIBLE);
-			
-			
-			super.onPostExecute(result);
-		}
-
-		@Override
-		protected void onPreExecute() {
-			mProgresssBar.setVisibility(View.VISIBLE);
-			listView.setVisibility(View.GONE);
-			super.onPreExecute();
-		}
+	private void uploadPhotoInfo() {
+		String gpsProvider = LocationManager.GPS_PROVIDER;
+//		String networkProvider = LocationManager.NETWORK_PROVIDER;
+		Location location = getLocationInfoByGps(gpsProvider);
+//		if(location==null) {
+//			getCellLocation(networkProvider);
+//		}
+	
 		
+		AvatarRequestParam params = new AvatarRequestParam();
+		params.setLocation(location);
 		
+		params.setPoints(pointlist);
+		params.setLabel(picTitle);
+		params.setEmail(SettingLoader.getRegEmail(this));
+		params.setPhotoid(SettingLoader.getPhotoId(this));
+//		params.setEmail("aaa@163.com");
+//		params.setPhotoid("12345256");
+//		params.setTipsid("23453576");//大头针
+		
+		AvatarApi api = new AvatarApi(this);
+		api.setReturnResultListener(new ApiReturnResultListener() {
+			
+			@Override
+			public <T> void onReturnSucceedResult(int requestCode,
+					ApiResult<T> apiResult) {
+				Log.e(TAG, "onReturnSucceedResult is runing");
+				ArrayList<Avatar> entities = (ArrayList<Avatar>) apiResult.getEntities();
+				if(entities!=null) {
+					mProgresssBar.setVisibility(View.GONE);
+					listView.setVisibility(View.VISIBLE);
+					tiptext.setVisibility(View.GONE);
+					
+					adapter = new ListAdapter(entities);
+					listView.setAdapter(adapter);
+					
+					listView.setOnItemClickListener(new OnItemClickListener() {
+
+						@Override
+						public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+								long arg3) {
+							if(editButton.isChecked()) {
+								Intent intent = new Intent(PicListActivity.this,RectActiivity.class);
+								Avatar avatar = list.get(arg2);
+								if(Utils.isNotNullOrEmpty(avatar.getPath())) {
+									intent.putExtra("mCurrentFile",avatar.getPath());
+									intent.putExtra("mFromPicList", true);
+									startActivity(intent);
+								}else {
+									Toast.makeText(PicListActivity.this, "没有可编辑的图片哦！", Toast.LENGTH_SHORT).show();
+								}
+							}else {
+								Intent intent = new Intent(PicListActivity.this,DishActivity.class);
+								startActivity(intent);
+							}
+						}
+					});
+				}else {
+					mProgresssBar.setVisibility(View.GONE);
+					listView.setVisibility(View.GONE);
+					tiptext.setVisibility(View.VISIBLE);
+					tiptext.setText("没有找到相关的信息哦！稍后再试吧！");
+				}
+				
+			}
+			
+			@Override
+			public <T> void onReturnFailResult(int requestCode, ApiResult<T> apiResult) {
+				// TODO Auto-generated method stub
+				mProgresssBar.setVisibility(View.GONE);
+				listView.setVisibility(View.GONE);
+				tiptext.setVisibility(View.VISIBLE);
+				tiptext.setText("没有找到相关的信息哦！稍后再试吧！");
+			}
+		});
+		api.sendAvatarInfo(1, params, 1, "25");
 		
 	}
-
-
+	
+	private Location getLocationInfoByGps(String provider) {
+		Location location = null;
+		LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		android.location.Location lastKnownLocation = locationManager .getLastKnownLocation(provider);
+		if(lastKnownLocation!=null) {
+			double Latitude = lastKnownLocation.getLatitude();
+			double Longitude = lastKnownLocation.getLongitude();
+			if(Latitude!=0&&Longitude!=0) {
+				location = new Location(Latitude+"", Longitude+"");
+			}
+		}
+	    return location;
+	}
+	
+   /*//GSM和Cdma
+	private Location getCellLocation(TelephonyManager manager) {
+		 CellLocation cellLocation = manager.getCellLocation();
+		 if(true){
+				GsmCellLocation gsm = ((GsmCellLocation) manager
+						.getCellLocation());
+				// 获取基站id与地区码
+				if (gsm != null) {
+					int cid = gsm.getCid();
+					int lac = gsm.getLac();
+//					locationInfo.setCellid(cid);
+//					locationInfo.setLac(lac);
+				}
+			} else if (cellLocation instanceof CdmaCellLocation) {
+				CdmaCellLocation cdma = ((CdmaCellLocation) manager
+						.getCellLocation());
+				// 获取基站的经纬度
+				if (cdma != null) {
+//					sid = cdma.getSystemId();
+//					bid = cdma.getBaseStationId();
+//					nid = cdma.getNetworkId();
+//					locationInfo.setBid(bid);
+//					locationInfo.setNid(nid);
+//					locationInfo.setSid(sid);
+				}
+			}
+		return null;
+	}
+*/
 	
 	private void initComponents() {
 		backButton = (Button)findViewById(R.id.btn_back);
@@ -81,31 +185,12 @@ public class PicListActivity extends BaseActivity implements View.OnClickListene
 		editButton = (CheckBox)findViewById(R.id.btn_edit);
 		
 		mProgresssBar = (ProgressBar)findViewById(R.id.progressBar1);
-		
+		tiptext = (TextView)findViewById(R.id.textView2);
 		listView = (ListView) findViewById(R.id.listView1);
-		listView.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				if(editButton.isChecked()) {
-					Intent intent = new Intent(PicListActivity.this,RectActiivity.class);
-					Avatar avatar = list.get(arg2);
-					if(Utils.isNotNullOrEmpty(avatar.getPath())) {
-						intent.putExtra("mCurrentFile",avatar.getPath());
-						intent.putExtra("mFromPicList", true);
-						startActivity(intent);
-					}else {
-						Toast.makeText(PicListActivity.this, "没有可编辑的图片哦！", Toast.LENGTH_SHORT).show();
-					}
-				}else {
-					Intent intent = new Intent(PicListActivity.this,DishActivity.class);
-					startActivity(intent);
-				}
-			}
-		});
-		ListAdapter adapter = new ListAdapter(loadData());
-		listView.setAdapter(adapter);
+		
+		mProgresssBar.setVisibility(View.VISIBLE);
+		listView.setVisibility(View.GONE);
+		tiptext.setVisibility(View.GONE);
 
 	}
 	
--- pictureshare/src/com/android/app/PicTitleActvity.java
+++ pictureshare/src/com/android/app/PicTitleActvity.java
@@ -4,29 +4,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.location.LocationManager;
 import android.media.ExifInterface;
 import android.os.Bundle;
-import android.telephony.CellLocation;
-import android.telephony.TelephonyManager;
-import android.telephony.cdma.CdmaCellLocation;
-import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.android.app.api.AvatarApi;
-import com.android.app.api.AvatarRequestParam;
-import com.android.app.data.SettingLoader;
-import com.android.app.entity.Location;
-import com.android.app.entity.Point;
 import com.android.app.utils.ImageUtil;
 import com.android.app.utils.Utils;
 
@@ -38,7 +26,6 @@ public class PicTitleActvity extends BaseActivity implements OnClickListener{
 	private ImageView confirmButton;
 	private String filePath;
 	private Bitmap bitmap;
-	private LocationManager locationManager;
 	private ArrayList<String> list;
 
 	@Override
@@ -48,7 +35,6 @@ public class PicTitleActvity extends BaseActivity implements OnClickListener{
 		
 		filePath = getIntent().getStringExtra("mCurrentFile");
 		list = getIntent().getStringArrayListExtra("list");
-		
 		
 		initComponents();
 		updateUI();
@@ -125,9 +111,8 @@ public class PicTitleActvity extends BaseActivity implements OnClickListener{
 			Intent intent = new Intent(this,PicListActivity.class);
 			intent.putExtra("picTitle", title);
 			intent.putExtra("mCurrentFile", filePath);
+			intent.putStringArrayListExtra("list", list);
 			startActivity(intent);
-			
-			uploadPhotoInfo();
 			break;
 
 		default:
@@ -136,73 +121,4 @@ public class PicTitleActvity extends BaseActivity implements OnClickListener{
 		
 	}
 
-	private void uploadPhotoInfo() {
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		String gpsProvider = LocationManager.GPS_PROVIDER;
-		String networkProvider = LocationManager.NETWORK_PROVIDER;
-		Location location = getLocationInfoByGps(gpsProvider);
-//		if(location==null) {
-//			getCellLocation(networkProvider);
-//		}
-		
-		String label = picTitle.getText().toString();
-		if(Utils.isNullOrEmpty(label)) {
-			Toast.makeText(this, "输入一个标题吧！亲。", Toast.LENGTH_SHORT).show();
-			return;
-		}
-		
-		AvatarRequestParam params = new AvatarRequestParam();
-		params.setLocation(location);
-		
-		params.setPoints(list);
-		params.setLabel(label);
-		params.setEmail(SettingLoader.getRegEmail(this));
-		params.setPhotoid(SettingLoader.getPhotoId(this));
-//		params.setEmail("aaa@163.com");
-//		params.setPhotoid("12345256");
-//		params.setTipsid("23453576");//大头针
-		
-		AvatarApi api = new AvatarApi(this);
-		api.sendAvatarInfo(1, params, 1, "25");
-		
-	}
-	
-	private Location getLocationInfoByGps(String provider) {
-		Location location = null;
-		double Latitude = locationManager.getLastKnownLocation(provider).getLatitude();
-		double Longitude = locationManager.getLastKnownLocation(provider).getLongitude();
-		if(Latitude!=0&&Longitude!=0) {
-			location = new Location(Latitude+"", Longitude+"");
-		}
-	    return location;
-	}
-	
-   //GSM和Cdma
-	private Location getCellLocation(TelephonyManager manager) {
-		 CellLocation cellLocation = manager.getCellLocation();
-		 if(true){
-				GsmCellLocation gsm = ((GsmCellLocation) manager
-						.getCellLocation());
-				// 获取基站id与地区码
-				if (gsm != null) {
-					int cid = gsm.getCid();
-					int lac = gsm.getLac();
-//					locationInfo.setCellid(cid);
-//					locationInfo.setLac(lac);
-				}
-			} else if (cellLocation instanceof CdmaCellLocation) {
-				CdmaCellLocation cdma = ((CdmaCellLocation) manager
-						.getCellLocation());
-				// 获取基站的经纬度
-				if (cdma != null) {
-//					sid = cdma.getSystemId();
-//					bid = cdma.getBaseStationId();
-//					nid = cdma.getNetworkId();
-//					locationInfo.setBid(bid);
-//					locationInfo.setNid(nid);
-//					locationInfo.setSid(sid);
-				}
-			}
-		return null;
-	}
 }
